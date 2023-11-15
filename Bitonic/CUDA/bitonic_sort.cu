9a89e91966372ec4dcb10828af759a3a99b676c5
kirthivel-ramesh <kirthivel.ramesh@tamu.edu>
2023-11-15 02:54:54 -0600

    bitonic caliper regions up

--- Bitonic/CUDA/bitonic_sort.cu
+++ Bitonic/CUDA/bitonic_sort.cu
@@ -19,6 +19,13 @@ int THREADS;
 int BLOCKS;
 int NUM_VALS;
 
+const char* main_time = "main";
+const char* data_init = "data_init";
+const char* comm_small = "comm_small_1";
+const char* comm_small_2 = "comm_small_2";
+const char* comp_small = "comp_small";
+const char* comp_large = "comp_large";
+const char* correct_check = "correct_check";
 const char* bitonic_sort_step_region = "bitonic_sort_step";
 const char* cudaMemcpy_host_to_device = "cudaMemcpy_host_to_device";
 const char* cudaMemcpy_device_to_host = "cudaMemcpy_device_to_host";
@@ -64,6 +71,8 @@ __global__ void bitonic_sort_step(float *dev_values, int j, int k)
   i = threadIdx.x + blockDim.x * blockIdx.x;
   ixj = i^j;
 
+  // CALI_MARK_BEGIN(comp_small);
+
   /* The threads with the lowest ids sort the array. */
   if ((ixj)>i) {
     if ((i&k)==0) {
@@ -85,6 +94,8 @@ __global__ void bitonic_sort_step(float *dev_values, int j, int k)
       }
     }
   }
+
+  // CALI_MARK_END(comp_small);
 }
 
 /**
@@ -92,6 +103,7 @@ __global__ void bitonic_sort_step(float *dev_values, int j, int k)
  */
 void bitonic_sort(float *values)
 {
+  CALI_MARK_BEGIN(main);
   float *dev_values;
   size_t size = NUM_VALS * sizeof(float);
 
@@ -103,6 +115,7 @@ void bitonic_sort(float *values)
   
   //MEM COPY FROM HOST TO DEVICE
   CALI_MARK_BEGIN(cudaMemcpy_host_to_device);
+  CALI_MARK_BEGIN(comm_small_1);
 
   cudaEventRecord(start1, 0);
 
@@ -111,6 +124,7 @@ void bitonic_sort(float *values)
   cudaEventRecord(stop1, 0);
   cudaEventSynchronize(stop1);
 
+  CALI_MARK_END(comm_small_1);
   CALI_MARK_END(cudaMemcpy_host_to_device);
 
   cudaEventElapsedTime(&cudaMemcpy_host_to_device_time, start1, stop1);
@@ -134,6 +148,8 @@ void bitonic_sort(float *values)
 
   cudaEventRecord(start2, 0);
 
+  CALI_MARK_BEGIN(comp_large);
+
   /* Major step */
   for (k = 2; k <= NUM_VALS; k <<= 1) {
     /* Minor step */
@@ -142,6 +158,8 @@ void bitonic_sort(float *values)
       bitonic_sort_step<<<blocks, threads>>>(dev_values, j, k);
     }
   }
+
+  CALI_MARK_END(comp_large);
 
   cudaEventRecord(stop2, 0);
   cudaEventSynchronize(stop2);
@@ -163,6 +181,7 @@ void bitonic_sort(float *values)
 
   //MEM COPY FROM DEVICE TO HOST
   CALI_MARK_BEGIN(cudaMemcpy_device_to_host);
+  CALI_MARK_BEGIN(comm_small_2);
 
   cudaEventRecord(start3, 0);
 
@@ -171,6 +190,7 @@ void bitonic_sort(float *values)
   cudaEventRecord(stop3, 0);
   cudaEventSynchronize(stop3);
 
+  CALI_MARK_END(comm_small_2);
   CALI_MARK_END(cudaMemcpy_device_to_host);
 
   cudaEventElapsedTime(&cudaMemcpy_device_to_host_time, start3, stop3);
@@ -187,7 +207,7 @@ void bitonic_sort(float *values)
   cudaEventDestroy(stop3);
 
   cudaFree(dev_values);
-
+  CALI_MARK_END(main_time);
 }
 
 int main(int argc, char *argv[])
@@ -207,13 +227,28 @@ int main(int argc, char *argv[])
 
   clock_t start, stop;
 
+  CALI_MARK_BEGIN(data_init);
+
   float *values = (float*) malloc( NUM_VALS * sizeof(float));
   array_fill(values, NUM_VALS);
+
+  CALI_MARK_END(data_init);
 
   start = clock();
   bitonic_sort(values); /* Inplace */
   stop = clock();
 
+  CALI_MARK_BEGIN(correct_check);
+
+  for (int i = 0; i < NUM_VALS - 1; i++) {
+    if (values[i] > values[i + 1]) {
+      printf("ERROR: Array not sorted correctly!\n");
+      break;
+    }
+  }
+
+  CALI_MARK_END(correct_check);
+  
   print_elapsed(start, stop);
 
 
