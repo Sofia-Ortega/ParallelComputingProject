819557a062a63504285eea7fd7a2d932e9b57c2a
Sriabhi Venkat <sriabhi01@gmail.com>
2023-11-15 16:41:38 -0600

    cali files

---
+++ quicksort/mpi/jobrunner.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+echo "Script starting"
+
+## 2^18
+# sbatch quicksort_mpi.grace_job 262144 2 0
+# sbatch quicksort_mpi.grace_job 262144 4 0
+# sbatch quicksort_mpi.grace_job 262144 8 0
+# sbatch quicksort_mpi.grace_job 262144 16 0
+# sbatch quicksort_mpi.grace_job 262144 32 0
+# sbatch quicksort_mpi.grace_job 262144 64 0
+# sbatch quicksort_mpi.grace_job 262144 128 0
+# sbatch quicksort_mpi.grace_job 262144 256 0
+# sbatch quicksort_mpi.grace_job 262144 512 0
+# sbatch quicksort_mpi.grace_job 262144 1024 0
+# sbatch quicksort_mpi.grace_job 262144 2 1
+# sbatch quicksort_mpi.grace_job 262144 4 1
+# sbatch quicksort_mpi.grace_job 262144 8 1
+# sbatch quicksort_mpi.grace_job 262144 16 1
+# sbatch quicksort_mpi.grace_job 262144 32 1
+# sbatch quicksort_mpi.grace_job 262144 64 1
+# sbatch quicksort_mpi.grace_job 262144 128 1
+# sbatch quicksort_mpi.grace_job 262144 256 1
+# sbatch quicksort_mpi.grace_job 262144 512 1
+# sbatch quicksort_mpi.grace_job 262144 1024 1
+# sbatch quicksort_mpi.grace_job 262144 2 2
+# sbatch quicksort_mpi.grace_job 262144 4 2
+# sbatch quicksort_mpi.grace_job 262144 8 2
+# sbatch quicksort_mpi.grace_job 262144 16 2
+# sbatch quicksort_mpi.grace_job 262144 32 2
+# sbatch quicksort_mpi.grace_job 262144 64 2
+# sbatch quicksort_mpi.grace_job 262144 128 2
+# sbatch quicksort_mpi.grace_job 262144 256 2
+# sbatch quicksort_mpi.grace_job 262144 512 2
+# sbatch quicksort_mpi.grace_job 262144 1024 2
+# sbatch quicksort_mpi.grace_job 262144 2 3
+# sbatch quicksort_mpi.grace_job 262144 4 3
+# sbatch quicksort_mpi.grace_job 262144 8 3
+# sbatch quicksort_mpi.grace_job 262144 16 3
+# sbatch quicksort_mpi.grace_job 262144 32 3
+# sbatch quicksort_mpi.grace_job 262144 64 3
+# sbatch quicksort_mpi.grace_job 262144 128 3
+# sbatch quicksort_mpi.grace_job 262144 256 3
+# sbatch quicksort_mpi.grace_job 262144 512 3
+# sbatch quicksort_mpi.grace_job 262144 1024 3
+
+# # 2^20
+# sbatch quicksort_mpi.grace_job 1048576 2 0
+# sbatch quicksort_mpi.grace_job 1048576 4 0
+# sbatch quicksort_mpi.grace_job 1048576 8 0
+# sbatch quicksort_mpi.grace_job 1048576 16 0 - timeout
+# sbatch quicksort_mpi.grace_job 1048576 32 0
+# sbatch quicksort_mpi.grace_job 1048576 64 0
+# sbatch quicksort_mpi.grace_job 1048576 128 0
+# sbatch quicksort_mpi.grace_job 1048576 256 0
+# sbatch quicksort_mpi.grace_job 1048576 512 0 - timeout
+# sbatch quicksort_mpi.grace_job 1048576 1024 0
+# sbatch quicksort_mpi.grace_job 1048576 2 1
+# sbatch quicksort_mpi.grace_job 1048576 4 1
+# sbatch quicksort_mpi.grace_job 1048576 8 1
+# sbatch quicksort_mpi.grace_job 1048576 16 1
+# sbatch quicksort_mpi.grace_job 1048576 32 1
+# sbatch quicksort_mpi.grace_job 1048576 64 1
+# sbatch quicksort_mpi.grace_job 1048576 128 1
+# sbatch quicksort_mpi.grace_job 1048576 256 1
+# sbatch quicksort_mpi.grace_job 1048576 512 1
+# sbatch quicksort_mpi.grace_job 1048576 1024 1
+# sbatch quicksort_mpi.grace_job 1048576 2 2
+# sbatch quicksort_mpi.grace_job 1048576 4 2
+# sbatch quicksort_mpi.grace_job 1048576 8 2
+# sbatch quicksort_mpi.grace_job 1048576 16 2
+# sbatch quicksort_mpi.grace_job 1048576 32 2
+# sbatch quicksort_mpi.grace_job 1048576 64 2
+# sbatch quicksort_mpi.grace_job 1048576 128 2
+# sbatch quicksort_mpi.grace_job 1048576 256 2
+# sbatch quicksort_mpi.grace_job 1048576 512 2
+# sbatch quicksort_mpi.grace_job 1048576 1024 2
+# sbatch quicksort_mpi.grace_job 1048576 2 3
+# sbatch quicksort_mpi.grace_job 1048576 4 3
+# sbatch quicksort_mpi.grace_job 1048576 8 3
+# sbatch quicksort_mpi.grace_job 1048576 16 3
+# sbatch quicksort_mpi.grace_job 1048576 32 3
+# sbatch quicksort_mpi.grace_job 1048576 64 3
+# sbatch quicksort_mpi.grace_job 1048576 128 3
+# sbatch quicksort_mpi.grace_job 1048576 256 3
+# sbatch quicksort_mpi.grace_job 1048576 512 3
+# sbatch quicksort_mpi.grace_job 1048576 1024 3
+
+# 2^22
+sbatch quicksort_mpi.grace_job 4194304 2 0
+sbatch quicksort_mpi.grace_job 4194304 4 0
+sbatch quicksort_mpi.grace_job 4194304 8 0
+sbatch quicksort_mpi.grace_job 4194304 16 0
+sbatch quicksort_mpi.grace_job 4194304 32 0
+sbatch quicksort_mpi.grace_job 4194304 64 0
+sbatch quicksort_mpi.grace_job 4194304 128 0
+sbatch quicksort_mpi.grace_job 4194304 256 0
+sbatch quicksort_mpi.grace_job 4194304 512 0
+sbatch quicksort_mpi.grace_job 4194304 1024 0
+sbatch quicksort_mpi.grace_job 4194304 2 1
+sbatch quicksort_mpi.grace_job 4194304 4 1
+sbatch quicksort_mpi.grace_job 4194304 8 1
+sbatch quicksort_mpi.grace_job 4194304 16 1
+sbatch quicksort_mpi.grace_job 4194304 32 1
+sbatch quicksort_mpi.grace_job 4194304 64 1
+sbatch quicksort_mpi.grace_job 4194304 128 1
+sbatch quicksort_mpi.grace_job 4194304 256 1
+sbatch quicksort_mpi.grace_job 4194304 512 1
+sbatch quicksort_mpi.grace_job 4194304 1024 1
+sbatch quicksort_mpi.grace_job 4194304 2 2
+sbatch quicksort_mpi.grace_job 4194304 4 2
+sbatch quicksort_mpi.grace_job 4194304 8 2
+sbatch quicksort_mpi.grace_job 4194304 16 2
+sbatch quicksort_mpi.grace_job 4194304 32 2
+sbatch quicksort_mpi.grace_job 4194304 64 2
+sbatch quicksort_mpi.grace_job 4194304 128 2
+sbatch quicksort_mpi.grace_job 4194304 256 2
+sbatch quicksort_mpi.grace_job 4194304 512 2
+sbatch quicksort_mpi.grace_job 4194304 1024 2 - timeout
+sbatch quicksort_mpi.grace_job 4194304 2 3 - timeout
+sbatch quicksort_mpi.grace_job 4194304 4 3
+sbatch quicksort_mpi.grace_job 4194304 8 3
+sbatch quicksort_mpi.grace_job 4194304 16 3
+sbatch quicksort_mpi.grace_job 4194304 32 3
+sbatch quicksort_mpi.grace_job 4194304 64 3
+sbatch quicksort_mpi.grace_job 4194304 128 3
+sbatch quicksort_mpi.grace_job 4194304 256 3
+sbatch quicksort_mpi.grace_job 4194304 512 3
+sbatch quicksort_mpi.grace_job 4194304 1024 3
+
+# # 2^24
+# sbatch quicksort_mpi.grace_job 16777216 2 0
+# sbatch quicksort_mpi.grace_job 16777216 4 0
+# sbatch quicksort_mpi.grace_job 16777216 8 0
+# sbatch quicksort_mpi.grace_job 16777216 16 0
+# sbatch quicksort_mpi.grace_job 16777216 32 0
+# sbatch quicksort_mpi.grace_job 16777216 64 0
+# sbatch quicksort_mpi.grace_job 16777216 128 0
+# sbatch quicksort_mpi.grace_job 16777216 256 0
+# sbatch quicksort_mpi.grace_job 16777216 512 0
+# sbatch quicksort_mpi.grace_job 16777216 1024 0
+# sbatch quicksort_mpi.grace_job 16777216 2 1
+# sbatch quicksort_mpi.grace_job 16777216 4 1
+# sbatch quicksort_mpi.grace_job 16777216 8 1
+# sbatch quicksort_mpi.grace_job 16777216 16 1
+# sbatch quicksort_mpi.grace_job 16777216 32 1
+# sbatch quicksort_mpi.grace_job 16777216 64 1
+# sbatch quicksort_mpi.grace_job 16777216 128 1
+# sbatch quicksort_mpi.grace_job 16777216 256 1
+# sbatch quicksort_mpi.grace_job 16777216 512 1
+# sbatch quicksort_mpi.grace_job 16777216 1024 1
+# sbatch quicksort_mpi.grace_job 16777216 2 2
+# sbatch quicksort_mpi.grace_job 16777216 4 2
+# sbatch quicksort_mpi.grace_job 16777216 8 2
+# sbatch quicksort_mpi.grace_job 16777216 16 2
+# sbatch quicksort_mpi.grace_job 16777216 32 2
+# sbatch quicksort_mpi.grace_job 16777216 64 2
+# sbatch quicksort_mpi.grace_job 16777216 128 2
+# sbatch quicksort_mpi.grace_job 16777216 256 2
+# sbatch quicksort_mpi.grace_job 16777216 512 2
+# sbatch quicksort_mpi.grace_job 16777216 1024 2
+# sbatch quicksort_mpi.grace_job 16777216 2 3
+# sbatch quicksort_mpi.grace_job 16777216 4 3
+# sbatch quicksort_mpi.grace_job 16777216 8 3
+# sbatch quicksort_mpi.grace_job 16777216 16 3
+# sbatch quicksort_mpi.grace_job 16777216 32 3
+# sbatch quicksort_mpi.grace_job 16777216 64 3
+# sbatch quicksort_mpi.grace_job 16777216 128 3
+# sbatch quicksort_mpi.grace_job 16777216 256 3
+# sbatch quicksort_mpi.grace_job 16777216 512 3
+# sbatch quicksort_mpi.grace_job 16777216 1024 3
+
+# # 2^26
+# sbatch quicksort_mpi.grace_job 67108864 2 0
+# sbatch quicksort_mpi.grace_job 67108864 4 0
+# sbatch quicksort_mpi.grace_job 67108864 8 0
+# sbatch quicksort_mpi.grace_job 67108864 16 0
+# sbatch quicksort_mpi.grace_job 67108864 32 0
+# sbatch quicksort_mpi.grace_job 67108864 64 0
+# sbatch quicksort_mpi.grace_job 67108864 128 0
+# sbatch quicksort_mpi.grace_job 67108864 256 0
+# sbatch quicksort_mpi.grace_job 67108864 512 0
+# sbatch quicksort_mpi.grace_job 67108864 1024 0
+# sbatch quicksort_mpi.grace_job 67108864 2 1
+# sbatch quicksort_mpi.grace_job 67108864 4 1
+# sbatch quicksort_mpi.grace_job 67108864 8 1
+# sbatch quicksort_mpi.grace_job 67108864 16 1
+# sbatch quicksort_mpi.grace_job 67108864 32 1
+# sbatch quicksort_mpi.grace_job 67108864 64 1
+# sbatch quicksort_mpi.grace_job 67108864 128 1
+# sbatch quicksort_mpi.grace_job 67108864 256 1
+# sbatch quicksort_mpi.grace_job 67108864 512 1
+# sbatch quicksort_mpi.grace_job 67108864 1024 1
+# sbatch quicksort_mpi.grace_job 67108864 2 2
+# sbatch quicksort_mpi.grace_job 67108864 4 2
+# sbatch quicksort_mpi.grace_job 67108864 8 2
+# sbatch quicksort_mpi.grace_job 67108864 16 2
+# sbatch quicksort_mpi.grace_job 67108864 32 2
+# sbatch quicksort_mpi.grace_job 67108864 64 2
+# sbatch quicksort_mpi.grace_job 67108864 128 2
+# sbatch quicksort_mpi.grace_job 67108864 256 2
+# sbatch quicksort_mpi.grace_job 67108864 512 2
+# sbatch quicksort_mpi.grace_job 67108864 1024 2
+# sbatch quicksort_mpi.grace_job 67108864 2 3
+# sbatch quicksort_mpi.grace_job 67108864 4 3
+# sbatch quicksort_mpi.grace_job 67108864 8 3
+# sbatch quicksort_mpi.grace_job 67108864 16 3
+# sbatch quicksort_mpi.grace_job 67108864 32 3
+# sbatch quicksort_mpi.grace_job 67108864 64 3
+# sbatch quicksort_mpi.grace_job 67108864 128 3
+# sbatch quicksort_mpi.grace_job 67108864 256 3
+# sbatch quicksort_mpi.grace_job 67108864 512 3
+# sbatch quicksort_mpi.grace_job 67108864 1024 3
+
+# # 2^28
+# sbatch quicksort_mpi.grace_job 268435456 2 0
+# sbatch quicksort_mpi.grace_job 268435456 4 0
+# sbatch quicksort_mpi.grace_job 268435456 8 0
+# sbatch quicksort_mpi.grace_job 268435456 16 0
+# sbatch quicksort_mpi.grace_job 268435456 32 0
+# sbatch quicksort_mpi.grace_job 268435456 64 0
+# sbatch quicksort_mpi.grace_job 268435456 128 0
+# sbatch quicksort_mpi.grace_job 268435456 256 0
+# sbatch quicksort_mpi.grace_job 268435456 512 0
+# sbatch quicksort_mpi.grace_job 268435456 1024 0
+# sbatch quicksort_mpi.grace_job 268435456 2 1
+# sbatch quicksort_mpi.grace_job 268435456 4 1
+# sbatch quicksort_mpi.grace_job 268435456 8 1
+# sbatch quicksort_mpi.grace_job 268435456 16 1
+# sbatch quicksort_mpi.grace_job 268435456 32 1
+# sbatch quicksort_mpi.grace_job 268435456 64 1
+# sbatch quicksort_mpi.grace_job 268435456 128 1
+# sbatch quicksort_mpi.grace_job 268435456 256 1
+# sbatch quicksort_mpi.grace_job 268435456 512 1
+# sbatch quicksort_mpi.grace_job 268435456 1024 1
+# sbatch quicksort_mpi.grace_job 268435456 2 2
+# sbatch quicksort_mpi.grace_job 268435456 4 2
+# sbatch quicksort_mpi.grace_job 268435456 8 2
+# sbatch quicksort_mpi.grace_job 268435456 16 2
+# sbatch quicksort_mpi.grace_job 268435456 32 2
+# sbatch quicksort_mpi.grace_job 268435456 64 2
+# sbatch quicksort_mpi.grace_job 268435456 128 2
+# sbatch quicksort_mpi.grace_job 268435456 256 2
+# sbatch quicksort_mpi.grace_job 268435456 512 2
+# sbatch quicksort_mpi.grace_job 268435456 1024 2
+# sbatch quicksort_mpi.grace_job 268435456 2 3
+# sbatch quicksort_mpi.grace_job 268435456 4 3
+# sbatch quicksort_mpi.grace_job 268435456 8 3
+# sbatch quicksort_mpi.grace_job 268435456 16 3
+# sbatch quicksort_mpi.grace_job 268435456 32 3
+# sbatch quicksort_mpi.grace_job 268435456 64 3
+# sbatch quicksort_mpi.grace_job 268435456 128 3
+# sbatch quicksort_mpi.grace_job 268435456 256 3
+# sbatch quicksort_mpi.grace_job 268435456 512 3
+# sbatch quicksort_mpi.grace_job 268435456 1024 3
+echo "Script execution finished"
